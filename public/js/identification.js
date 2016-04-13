19357d5c67c69c26298c8860f8393ac43ea68d25
karthikbadam <sriram.karthik6@gmail.com>
2016-04-12 23:43:10 -0400

    identify

--- public/js/identification.js
+++ public/js/identification.js
@@ -1,20 +1,21 @@
-function Identification (options) {
-    
-    var _self = this; 
-    
+function Identification(options) {
+
+    var _self = this;
+
     _self.cols = options.cols;
-    
+
     _self.data = options.data;
-    
+
     _self.dataTypes = findDataTypes();
-    
-    
+
+
 }
 
 var findDataTypes = function () {
 
     var dataTypes = {};
-    
+    var isNumeric = {};
+
     var data = _self.data;
 
     var cols = Object.keys(data[0]);
@@ -27,44 +28,45 @@ var findDataTypes = function () {
 
             var value = data[i][key];
 
-            if (value == "" || value == "NaN" 
-                || value == "undefined") {
+            if (value == "" || value == "NaN" || value == "undefined") {
 
                 continue;
 
             } else {
 
                 isNumeric[key] = checkNumeric(value);
-                
+
                 if (checkNumeric(value) == true) {
-                    
+
                     dataTypes[key] = "number";
-                    
+
                 } else {
-                    
+
                     dataTypes[key] = "string";
-                    
+
                 }
             }
-            
+
             if (key.toLowerCase().indexOf("date") >= 0) {
-                
+
                 isNumeric[key] = checkNumeric(value);
-                
+
                 dataTypes[key] = "date";
-                
-            } else if (key.toLowerCase().indexOf("time") 
-                       >= 0) {
-                
+
+            } else if (key.toLowerCase().indexOf("time") >= 0) {
+
                 isNumeric[key] = checkNumeric(value);
-                
+
                 dataTypes[key] = "time";
             }
-            
+
         }
     }
-    
-    return dataTypes;
+
+    return {
+        "type": dataTypes,
+        "check": isNumeric
+    };
 }
 
 function checkNumeric(n) {
